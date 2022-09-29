9a6e100d3d2094f4b5e7a73f3170ad93f2b1bf8e
Peter <Pete5x5@users.noreply.github.com>
2022-09-28 23:10:54 -0400

    switch to new dark mode

--- dark.js
+++ dark.js
@@ -1,27 +1,42 @@
-const themes = ["light", "dark"];
-const button = document.querySelector("#theme-switcher");
+const setColorMode = (mode) => {
+	// Mode was given
+	if (mode) {
+		// Update data-* attr on html
+		document.documentElement.setAttribute('data-force-color-mode', mode);
+		// Persist in local storage
+		window.localStorage.setItem('color-mode', mode);
+		// Make sure the checkbox is up-to-date
+		document.querySelector('#toggle-darkmode').checked = (mode === 'dark');
+	}
+	
+	// No mode given (e.g. reset)
+	else {
+		// Remove data-* attr from html
+		document.documentElement.removeAttribute('data-force-color-mode');
+		// Remove entry from local storage
+		window.localStorage.removeItem('color-mode');
+		// Make sure the checkbox is up-to-date, matching the system preferences
+		document.querySelector('#toggle-darkmode').checked = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	}
+}
 
-const getThemeCurrent = () => document.documentElement.dataset.theme;
-const getThemeNext = () => {
-  const themeCurrent = getThemeCurrent();
-  const indexThemeCurrent = themes.indexOf(themeCurrent);
-  return themes[(indexThemeCurrent + 1) % themes.length];
-};
+document.querySelector('#toggle-darkmode').addEventListener('click', (e) => {
+	setColorMode(e.target.checked ? 'dark' : 'light');
+});
 
-// const updateIcon = () => {
-//   const themeCurrent = getThemeCurrent();
-//   document
-//     .querySelector(\`#icon-theme-\${themeCurrent}\`)
-//     .classList.add("hidden");
-//   const themeNext = getThemeNext();
-//   document
-//     .querySelector(\`#icon-theme-\${themeNext}\`)
-//     .classList.remove("hidden");
-// };
+document.querySelector('#reset-darkmode').addEventListener('click', (e) => {
+	e.preventDefault();
+	setColorMode(false);
+});
 
-button.addEventListener("click", () => {
-  const themeNext = getThemeNext();
-  document.documentElement.dataset.theme = themeNext;
-  localStorage.setItem("theme", themeNext);
-  updateIcon();
+// Keep an eye out for System Light/Dark Mode Changes
+const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+mediaQuery.addListener(() => {
+	// Ignore change if there's an override set
+	if (document.documentElement.getAttribute('data-force-color-mode')) {
+		return;
+	}
+
+	// Make sure the checkbox is up-to-date
+ 	document.querySelector('#toggle-darkmode').checked = mediaQuery.matches;
 });
